2214b5aabc18494186c614f8714839303c49ecd8
borer <janu.yieg.borer@gmail.com>
2021-05-26 19:51:49 +0800

    rebuilding site Wed May 26 07:51:49 PM CST 2021

---
+++ .profile
@@ -0,0 +1,5 @@
+. "$HOME/.cargo/env"
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS="@im=fcitx"
+nohup unclutter -idle 10 &
--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ ln -sr ./.vimrc $HOME/.vimrc
 ln -sr ./.zshrc $HOME/.zshrc
 ln -sr ./.keynavrc $HOME/.keynavrc
 ln -sr ./.tmux.conf $HOME/.tmux.conf
+ln -sr ./.profile $HOME/.profile
